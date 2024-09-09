dc97647dc71e70fa873a63ac805e610cd7f67cb1
Ikram <ikramali3752@gmail.com>
2024-09-09 10:43:28 +0500

    upload

---
+++ CRUD OperationsApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CRUD_OperationsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CRUD_OperationsApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ CRUD OperationsApi/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using CRUD_OperationsApi.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace MyCrudApp.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
